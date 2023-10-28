198b6d413c75b2e65e97b77571a8df8707b399f1
BartoszBrodowski <bartolini10058@gmail.com>
2023-10-28 21:40:08 +0200

    feat: dockerize API

--- src/config/connector.ts
+++ src/config/connector.ts
@@ -1,9 +1,9 @@
-import { Session } from 'neo4j-driver';
-import neo4j from 'neo4j-driver';
-import { USER, PASSWORD } from './variables';
+import { Session } from "neo4j-driver";
+import neo4j from "neo4j-driver";
+import { USER, PASSWORD } from "./variables";
 
 const driver = neo4j.driver(
-	'neo4j+s://a29d84d8.databases.neo4j.io',
-	neo4j.auth.basic(USER, PASSWORD)
+  "neo4j+s://a29d84d8.databases.neo4j.io",
+  neo4j.auth.basic(USER, PASSWORD)
 );
 export const session: Session = driver.session();
