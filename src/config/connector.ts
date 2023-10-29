876c982387e423b1d2b1d63e33809ffe0134415d
BartoszBrodowski <bartolini10058@gmail.com>
2023-10-29 19:53:26 +0100

    feat: dockerized db and connection

--- src/config/connector.ts
+++ src/config/connector.ts
@@ -1,9 +1,4 @@
-import { Session } from "neo4j-driver";
-import neo4j from "neo4j-driver";
-import { USER, PASSWORD } from "./variables";
+import neo4j, { Session } from "neo4j-driver";
 
-const driver = neo4j.driver(
-  "neo4j+s://a29d84d8.databases.neo4j.io",
-  neo4j.auth.basic(USER, PASSWORD)
-);
+const driver = neo4j.driver("neo4j://localhost:7687");
 export const session: Session = driver.session();
--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,25 @@
-import express, { Request, Response } from 'express';
-import usersRoutes from './routes/userRoutes';
-import cors from 'cors';
-import swaggerUi from 'swagger-ui-express';
-import swaggerDocument from '../swagger-output.json';
+import express, { Request, Response } from "express";
+import usersRoutes from "./routes/userRoutes";
+import cors from "cors";
+import swaggerUi from "swagger-ui-express";
+import swaggerDocument from "../swagger-output.json";
+
+const PORT = 5000;
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req: Request, res: Response) => {
-	res.send('Hello World!');
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World!");
 });
 
-app.use('/users', usersRoutes);
-app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use("/users", usersRoutes);
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-const server = app.listen(3000, () => {
-	console.log('Server started on port 3000');
+const server = app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
 });
 
 export { app, server };
